b52f974351b4fbc3b211fb90a5a65f77eabbac9d
reaovyd <zackbellzokobell@gmail.com>
2024-02-29 03:58:41 -0500

    feat(client): add code to write piece to piece file

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use bittorrent_starter_rust::{
 };
 use clap::Parser;
 use reqwest::Client;
+use tokio::{fs::OpenOptions, io::AsyncWriteExt};
 mod cli;
 
 #[tokio::main]
@@ -85,85 +86,13 @@ async fn main() -> Result<()> {
             let mut downloader =
                 Downloader::new(&client, 6881, torrent_file, peer_id, Compact::Compact).await?;
             let bytes = downloader.download_piece(piece_num).await?;
-            println!("{:?}", bytes);
-            // let client = Client::new();
-            // let (mut url, info) = from_file(torrent_file)?;
-            // let mut listener: Option<TcpListener> = None;
-            // for port in 6881..=6889 {
-            //     match TcpListener::bind(format!("127.0.0.1:{port}")).await {
-            //         Ok(l) => {
-            //             listener = Some(l);
-            //             break;
-            //         }
-            //         Err(_) => continue,
-            //     }
-            // }
-            // if let Some(listener) = listener {
-            //     let addr = listener.local_addr()?;
-            //     let query = QueryStringBuilder::new(
-            //         &info.info_hash()?,
-            //         b"00112233445566778899",
-            //         addr.port(),
-            //         0,
-            //         0,
-            //         match info.file_type() {
-            //             FileType::SingleFile(file) => *file,
-            //             FileType::MultiFile(_) => todo!(),
-            //         },
-            //         Compact::Compact,
-            //     )
-            //     .build();
-            //     url.set_query(Some(&query));
-            //     let req = client.get(url).build()?;
-            //     println!("{:?}", req.url());
-            //     let bytes = client.execute(req).await?.bytes().await?;
-            //     println!("{:?}", bytes);
-            //     let resp = TrackerResponse::from_bytes(&bytes)?;
-            //     // let peer = resp.peers().first().ok_or(anyhow!("No peer found!"))?;
-            //     let request_body = Handshake::new(&info.info_hash()?, b"00112233445566778899");
-            //     let piece_length = info.piece_length();
-            //     let body = &request_body.clone().into_bytes()[..];
-            //     for peer in resp.peers() {
-            //         let stream = TcpStream::connect(peer).await?;
-            //         let (mut reader, mut writer) = stream.into_split();
-            //         writer.write_all(body).await?;
-            //         let mut buf = [0; HANDSHAKE_SIZE];
-            //         reader.read_exact(&mut buf).await?;
-            //         let handshake = Handshake::from_bytes(&buf)?;
-            //         if request_body == handshake {
-            //             // handshake successful but nothing to check if it is a peer_id we want
-            //             // for now
-
-            //             let mut buf = [0; 10];
-            //             reader.read_exact(&mut buf).await?;
-            //             println!("{:?}", buf);
-            //             break;
-            //         }
-            //     }
-            // } else {
-            //     // NOTE: Could not listen on a Bittorrent port
-            // }
-
-            // let request_body = Handshake::new(&info.info_hash()?, b"00112233445566778899");
-            // let stream = TcpStream::connect(peer_addr).await?;
-            // let (mut reader, mut writer) = stream.into_split();
-            // let body = &mut request_body.clone().into_bytes()[..];
-            // writer.write_all(body).await?;
-            // let mut buf = [0; HANDSHAKE_SIZE];
-            // reader.read_exact(&mut buf).await?;
-            // let handshake = Handshake::from_bytes(&buf)?;
-            // assert_eq!(request_body, handshake);
-            // When a peer finishes downloading a piece and checks that the hash matches,
-            // it announces that it has that piece to all of its peers.
-            //
-            // Each peer connections two bits of state on both of the ends:
-            // 1. Choked or Not choked (in which it is unchoking)
-            //  - Choking is a notification that no data will be sent until unchoking
-            //    occurs
-            // 2. Interested or Not Interested
-            // Data transfer ONLY takes place if and only if
-            // 1. The side sending data is unchoking
-            // 2. The side receiving data is interested
+            let mut file = OpenOptions::new()
+                .write(true)
+                .read(true)
+                .create_new(true)
+                .open(out_file)
+                .await?;
+            file.write_all(&bytes).await?;
         }
     };
     Ok(())
